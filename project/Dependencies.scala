6fcfcce1fd61029e1a9ed269a26a33a4d1d0f173
Sacha Garnier <44492283+sachgar@users.noreply.github.com>
2023-06-05 16:45:45 +0200

    scala2 version updated

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,7 +1,7 @@
 object Dependencies {
   // Sync with GA (.github/workflows/build-test.yml)
-  val Scala212 = "2.12.17" // sync! see comment above
-  val Scala213 = "2.13.10" // sync! see comment above
+  val Scala212 = "2.12.18" // sync! see comment above
+  val Scala213 = "2.13.11" // sync! see comment above
   val Scala3 = "3.3.0-RC6"
   val AkkaVersion = "2.6.20"
   val PlayVersion = "2.9.0-M4"

d7e05d4483108a677355046e6142dfafeca877f1
Matthias Kurz <m.kurz@irregular.at>
2025-07-05 01:38:05 +0200

    scala-library 2.12.20 (was 2.12.18)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,6 +1,6 @@
 object Dependencies {
   // Sync with GA (.github/workflows/build-test.yml)
-  val Scala212 = "2.12.18" // sync! see comment above
+  val Scala212 = "2.12.20" // sync! see comment above
   val Scala213 = "2.13.12" // sync! see comment above
 
   val AkkaVersion = "2.5.32"
